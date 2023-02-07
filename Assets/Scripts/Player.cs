49c313e122778394054f0bbea73dbaa397c0d286
Avratz <signos97@gmail.com>
2023-02-07 19:04:05 -0300

    refactoring input system, decoupling input logic from player class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,32 +6,15 @@ public class Player : MonoBehaviour
 {
   [SerializeField]
   private float movementSpeed = 5f;
+  [SerializeField]
+  private GameInput gameInput;
 
   private bool isWalking = false;
 
   private void Update()
   {
-    Vector2 inputVector = new Vector2(0, 0);
-    if (Input.GetKey(KeyCode.W))
-    {
-      inputVector.y = 1;
-    }
-    if (Input.GetKey(KeyCode.S))
-    {
-      inputVector.y = -1;
-    }
+    Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-    if (Input.GetKey(KeyCode.A))
-    {
-      inputVector.x = -1;
-    }
-
-    if (Input.GetKey(KeyCode.D))
-    {
-      inputVector.x = 1;
-    }
-
-    inputVector = inputVector.normalized;
     Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
     transform.position += movementDirection * movementSpeed * Time.deltaTime;
 
