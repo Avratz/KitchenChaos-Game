ff3db304dbb1ea6a26a7adce692c1845de0dac4f
Avratz <signos97@gmail.com>
2023-02-07 18:21:30 -0300

    Walk/Idle animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
   [SerializeField]
   private float movementSpeed = 5f;
 
+  private bool isWalking = false;
+
   private void Update()
   {
     Vector2 inputVector = new Vector2(0, 0);
@@ -33,7 +35,14 @@ public class Player : MonoBehaviour
     Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
     transform.position += movementDirection * movementSpeed * Time.deltaTime;
 
+    isWalking = movementDirection != Vector3.zero;
+
     float rotateSpeed = 10f;
     transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotateSpeed);
+  }
+
+  public bool IsWalking
+  {
+    get => isWalking;
   }
 }
