7c2e92f16f17471f4602478c683212969d1b7e6b
Avratz <signos97@gmail.com>
2023-02-08 17:58:10 -0300

    Spawn object, working with Scriptable objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+  [SerializeField]
+  private KitchenObjectSO kitchenObjectSO;
+  [SerializeField]
+  private Transform counterTopPoint;
+
   public void Interact()
   {
-    Debug.Log("Clear counter");
+    Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+    kitchenObjectTransform.localPosition = Vector3.zero;
+
+    Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().KitchenObjectSO.objectName);
   }
 }
